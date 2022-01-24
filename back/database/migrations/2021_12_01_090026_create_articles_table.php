72334b23193ca4cd860a14c00b8f70298a58ad91
Xin JIANG <xin.jiang@epitech.eu>
2022-01-24 21:28:18 +0100

    init

---
+++ back/database/migrations/2021_12_01_090026_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_owner');
+            $table->string('url');
+            $table->string('title');
+            $table->string('url_image');
+            $table->integer('id_categorie');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
